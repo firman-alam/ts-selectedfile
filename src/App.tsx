89fce012ddbaed975520fee634e99dea999b533e
firman-alam <aladiat046@gmail.com>
2022-09-23 20:46:51 +0700

    learn ts by creating single and multiple select

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Select, { SelectOption } from './Select';
+
+const options = [
+  { label: 'first', value: 1 },
+  { label: 'second', value: 2 },
+  { label: 'thirth', value: 3 },
+  { label: 'fourth', value: 4 },
+  { label: 'fifth', value: 5 },
+];
+
+function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
+  return (
+    <>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(option) => setValue1(option)}
+      />
+      <Select
+        options={options}
+        value={value2}
+        onChange={(option) => setValue2(option)}
+      />
+    </>
+  );
+}
+
+export default App;
